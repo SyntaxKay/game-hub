990b27c17a8f61e130ab3b1c07f5f90a36dcf67a
System Administrator <root@WKMZTD747C7E.global.publicisgroupe.net>
2025-10-28 11:23:02 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import * as React from "react";
-import { Button, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid, Text } from "@radix-ui/themes";
 import "./App.css";
 
 function App() {
   return (
     <>
-    <Text>
-      Hello, Game Hub!
-    </Text>
-    <Heading size="9">Welcome to Game Hub</Heading>
-    <Button>Roboto Button</Button>
+      <Grid
+        columns={{ initial: "1", sm: "2" }}
+        gap="3"
+        rows="repeat(2, auto)"
+        width="auto"
+      >
+        <Box>
+          <Text>nav</Text>
+        </Box>
+        <Box display={{ initial: "none", sm: "block" }}>
+          <Text>nav</Text>
+        </Box>
+        <Box display={{ initial: "none", sm: "block" }}>
+          <Text>aside</Text>
+        </Box>
+        <Box>
+          <Text>main</Text>
+        </Box>
+      </Grid>
     </>
   );
 }
