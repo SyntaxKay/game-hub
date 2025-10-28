ff3887498894867e30beeaedff20ab39661d5a5e
Kajal Dilipsinh <kajdilip@WKMZTD747C7E.global.publicisgroupe.net>
2025-10-28 18:36:12 +0530

    dark theme implemented

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Theme accentColor="indigo" grayColor="sand" radius="large" scaling="95%">
+    <Theme accentColor="indigo" grayColor="sand" radius="large" scaling="95%" appearance="dark">
       <App />
       {/* <ThemePanel /> */}
     </Theme>
