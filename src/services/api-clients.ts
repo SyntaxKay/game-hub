800e949409fe97b6285725ebc9239256568c5930
SyantaxKay <kajal10395@gmail.com>
2025-10-30 11:17:09 +0530

    custom hook created for fetching data

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+const RAWG_API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+
+if (!RAWG_API_KEY) {
+    throw new Error("RAWG API key is not defined in environment variables");
+}
+
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "e3fce895bfe64d0391501d26d7befda8",
-  },
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: RAWG_API_KEY,
+    },
 });
